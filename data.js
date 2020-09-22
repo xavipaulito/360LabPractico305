4fecd7bb21d524dbc8fa8a4073759ef69a572dc2
xavipaulito <67292631+xavipaulito@users.noreply.github.com>
2020-09-22 20:28:25 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,191 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20200921_164213_12",
+      "name": "PANO_20200921_164213_12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.506740005896516,
+        "pitch": 0.05623994836033219,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.508974042258373,
+          "pitch": 0.11521591626426897,
+          "rotation": 6.283185307179586,
+          "target": "1-pano_20200921_161811_5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.042721104997373,
+          "pitch": -0.07012471491290384,
+          "title": "Lockers",
+          "text": "Aquí deberán dejarse bolsos y mochilas, de forma de ingresar al laboratorio solamente con los útiles indispensables."
+        },
+        {
+          "yaw": 2.882960040238739,
+          "pitch": -0.11608213738290374,
+          "title": "Ducha de emergencia",
+          "text": ""
+        },
+        {
+          "yaw": -2.4474797255263727,
+          "pitch": -0.14296428870529887,
+          "title": "Laboratorio práctico 305",
+          "text": ""
+        },
+        {
+          "yaw": 0.010722152672729024,
+          "pitch": 0.05420838331743383,
+          "title": "Extintor",
+          "text": ""
+        },
+        {
+          "yaw": -0.15037442430765147,
+          "pitch": -0.03344597197369836,
+          "title": "Baños",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-pano_20200921_161811_5",
+      "name": "PANO_20200921_161811_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.029163145719767414,
+          "pitch": 0.20191878296213694,
+          "rotation": 0,
+          "target": "0-pano_20200921_164213_12"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0610259107353324,
+          "pitch": 0.45322897674831886,
+          "title": "Mesas de trabajo",
+          "text": ""
+        },
+        {
+          "yaw": 2.237233291811507,
+          "pitch": 0.11387182854401345,
+          "title": "Depósito de agua destilada",
+          "text": "Aquí se podrá extraer agua destilada para preparación de soluciones y lavado de material de vidrio."
+        },
+        {
+          "yaw": 2.7847289705585236,
+          "pitch": 0.31926791419288847,
+          "title": "Armario de ácidos",
+          "text": "Aquí se encuentran las soluciones de ácidos concentrados."
+        },
+        {
+          "yaw": 1.5025064243507114,
+          "pitch": 0.09415843976958804,
+          "title": "Zona de balanzas",
+          "text": "Esta mesada es reservada para las balanzas. Recomendamos no apoyar aquí cuadernos y demás objetos personales, dado que es una zona potencialmente \"contaminada\" con restos de reactivos. Se debe únicamente apoyar aquí los recipientes e instrumentos necesarios para realizar la transferencia de los reactivos masados y preparar soluciones de trabajo."
+        },
+        {
+          "yaw": 2.0843958339235753,
+          "pitch": 0.0835533851176784,
+          "title": "Zona de secado",
+          "text": "Aquí se depositaran ordenadamente los materiales de vidrio lavados luego de cada clase de laboratorio."
+        },
+        {
+          "yaw": 0.6161835456708928,
+          "pitch": -0.18690774622469775,
+          "title": "Campanas de extracción",
+          "text": "<span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">La usaremos para limitar la exposición a sustancias químicas peligrosas o nocivas, humos, vapores o polvos.&nbsp;</span><br><div><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\"><br></span></div>"
+        },
+        {
+          "yaw": -0.4932902064058471,
+          "pitch": -0.014796789483863648,
+          "title": "Armario de material de vidrio",
+          "text": ""
+        },
+        {
+          "yaw": -2.861984154065606,
+          "pitch": 0.12329325679647773,
+          "title": "Zona de lavado",
+          "text": ""
+        },
+        {
+          "yaw": 0.7536195040505884,
+          "pitch": 0.33222270700928114,
+          "title": "Mesa de trabajo",
+          "text": ""
+        },
+        {
+          "yaw": 1.1180797443287904,
+          "pitch": -0.03332874989376933,
+          "title": "Heladera",
+          "text": "Aquí se encontrarán los reactivos que requieran refrigeración."
+        },
+        {
+          "yaw": 2.409941299811557,
+          "pitch": 0.15991012396147752,
+          "title": "Zona de lavado",
+          "text": "Luego de cada práctica, todo el material utilizado deberá ser lavado con agua destilada para su posterior uso."
+        },
+        {
+          "yaw": 2.8944101900050914,
+          "pitch": 0.07455344185376589,
+          "title": "Secador para pipetas",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
